f942a604c59f3d3f912c204e29372bdcf63896ea
justin <justin8953@gmail.com>
2019-02-12 23:03:11 +0000

    delete not use var

--- web_front/my-app/src/Page/HomeComponent/Home.js
+++ web_front/my-app/src/Page/HomeComponent/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-import {Navbar,NavDropdown,Nav ,Row,Col} from 'react-bootstrap'
+import {Navbar,NavDropdown,Nav} from 'react-bootstrap'
 const Home = () => (
   <div>
     <h2>Home</h2>
