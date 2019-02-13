601063f977c65430c09afed57d0322ee4556a4c7
justin <justin8953@gmail.com>
2019-02-13 00:28:08 +0000

    run build

--- web/app.js
+++ web/app.js
@@ -18,7 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../web_front/my-app/build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../web_front/my-app/build', 'index.html'));
+});
+
 
 app.use('/', indexRouter);
 
--- web_front/my-app/src/App.js
+++ web_front/my-app/src/App.js
@@ -1,20 +1,24 @@
-import React from "react";
+import React, {Component }from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { About } from './Page/AboutComonent';
 import { Society, Home } from './Page/HomeComponent';
 import { Information} from './Page/InformationComponent';
 import { SecondHand } from "./Page/SecondHandComponent";
-const App = () => (
-  
-  <Router>
+class App extends Component{
+  render(){
+    return(
+      <Router>
     <div>
+      
       <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
+      <Route path="/about" component={About} />
       <Route path="/information" component={Information} />
       <Route path="/society" component={Society} />
       <Route path="/secondhand" component={SecondHand} />
     </div>
   </Router>
-);
+    );
+  }
+}
 
 export default App;
