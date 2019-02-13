85a0e3d6771160ad5e825695551ad5ecc1954109
justin <justin8953@gmail.com>
2019-02-13 10:42:59 +0000

    connect with server

--- web_front/my-app/src/App.js
+++ web_front/my-app/src/App.js
@@ -5,18 +5,31 @@ import { Society, Home } from './Page/HomeComponent';
 import { Information} from './Page/InformationComponent';
 import { SecondHand } from "./Page/SecondHandComponent";
 class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {apiResponse:""};
+  }
+  callAPI (){
+    fetch("http://localhost:9000/testAPI")
+    .then(res=>res.text())
+    .then(res=>this.setState({apiResponse:res}))
+    .catch(err=>err);
+  }
+  componentWillMount(){
+    this.callAPI();
+  }
   render(){
     return(
       <Router>
-    <div>
-      
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/information" component={Information} />
-      <Route path="/society" component={Society} />
-      <Route path="/secondhand" component={SecondHand} />
-    </div>
-  </Router>
+      <div>
+        <p>{this.state.apiResponse}</p>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/information" component={Information} />
+        <Route path="/society" component={Society} />
+        <Route path="/secondhand" component={SecondHand} />
+      </div>
+      </Router>
     );
   }
 }
