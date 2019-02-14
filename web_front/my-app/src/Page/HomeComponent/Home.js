8fcf488eb31a1d645b42819334a79df03c460db5
justin <justin8953@gmail.com>
2019-02-14 09:57:43 +0000

    solve conflict

--- web_front/my-app/src/Page/HomeComponent/Home.js
+++ web_front/my-app/src/Page/HomeComponent/Home.js
@@ -1,35 +1,8 @@
 import React from 'react'
-<<<<<<< HEAD
-import {Container,Row,Col,Form, Button, ButtonToolbar} from 'react-bootstrap'
+import {Container,Row,Col,Form,Button, ButtonToolbar} from 'react-bootstrap'
 import './Home.css'
-=======
-import { Container, Row, Col, Navbar, NavDropdown, Nav, Form, FormControl, Button } from 'react-bootstrap'
->>>>>>> b76195c60e5cdff213bb74614c0173ab2ba6b370
 const Home = (props) => (
   <div>
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Bristol Social Network</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/information">Information Board</Nav.Link>
-          <Nav.Link href="/society">Society Board</Nav.Link>
-          <Nav.Link href="/secondhand">Second-Hand Board</Nav.Link>
-          <Nav.Link href="/about">About Us</Nav.Link>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <NavDropdown title="Settings" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#">My Profile</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Message</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Preference</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
     <Container>
       <Row className="justify-content-md-center">
         <Col sm= {3}>
