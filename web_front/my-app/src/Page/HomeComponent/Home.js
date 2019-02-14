d089c7d534c3e0a1728f7228c6890628fe4f385b
justin <justin8953@gmail.com>
2019-02-14 09:46:49 +0000

    design home

--- web_front/my-app/src/Page/HomeComponent/Home.js
+++ web_front/my-app/src/Page/HomeComponent/Home.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import {Container,Row,Col} from 'react-bootstrap'
+import {Container,Row,Col,Form, Button, ButtonToolbar} from 'react-bootstrap'
+import './Home.css'
 const Home = () => (
   <div>
     <Container>
       <Row className="justify-content-md-center">
-        <Col>
+        <Col sm= {3}>
           <div>Text Here</div>
         </Col>
-        <Col>
-          <div>
-            Post Here
-          </div>
+        <Col sm={9} >
+          <Row className="justify-content-md-center">
+            <Form>
+              <Form.Group controlId="formTextarea">
+                <Form.Label>Posting</Form.Label>
+                <Form.Control placeholder="Share something today" as="textarea" rows="3" cols="80"/>
+                <ButtonToolbar>
+                  <Button variant="outline-secondary">Film</Button>
+                  <Button variant="outline-secondary">Image</Button>
+                </ButtonToolbar>
+              </Form.Group>
+            </Form>
+          </Row>
+          <Row>
+
+          </Row>
         </Col>
       </Row>
     </Container>
