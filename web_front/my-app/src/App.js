a676793117829e3360db87bff0cbd490e80058ed
davidchen <david2003542@gmail.com>
2019-02-15 12:40:48 +0000

    revise signup

--- web_front/my-app/src/App.js
+++ web_front/my-app/src/App.js
@@ -4,6 +4,7 @@ import { About } from './Page/AboutComponent';
 import { Society, Home } from './Page/HomeComponent';
 import { Information} from './Page/InformationComponent';
 import { SecondHand } from "./Page/SecondHandComponent";
+import { Signup } from "./Page/SignComponent";
 
 
 class App extends Component{
@@ -30,6 +31,7 @@ class App extends Component{
           <Route path="/information" component={Information} />
           <Route path="/society" component={Society} />
           <Route path="/secondhand" component={SecondHand} />
+          <Route path="/signup" component={Signup} />
         </Home>
         </div>
       </Router>
--- web_front/my-app/src/Page/SignComponent/Signup.js
+++ web_front/my-app/src/Page/SignComponent/Signup.js
@@ -1,10 +1,63 @@
 import React from 'react'
-import Detail from './Detail'
+import { Form, Button } from 'react-bootstrap'
+
+let state = {
+  ForeName:"",
+  LastName:"",
+  userName:"",
+  birth:"",
+  email:"",
+  password:""
+}
+
+let handleForeNameChange = (foreName) => {
+  this.state.foreName = foreName
+  console.log(state)
+}
+
+
+let handleSubmit = () => {
+  console.log('submit')
+}
 
 const Signup = () => (
-  <div>
-    <Detail></Detail>
-  </div>
+  <Form>
+    <Form.Group controlId="formForeName">
+    <Form.Label>foreName</Form.Label>
+    <Form.Control type="text" placeholder="Enter foreName" onChange={handleForeNameChange} />
+    </Form.Group>
+
+    <Form.Group controlId="formLastName">
+    <Form.Label>lastName</Form.Label>
+    <Form.Control type="text" placeholder="Enter lastName" />
+    </Form.Group>
+
+    <Form.Group controlId="userName">
+    <Form.Label>userName</Form.Label>
+    <Form.Control type="text" placeholder="Enter userName" />
+    </Form.Group>
+
+    <Form.Group controlId="birth">
+    <Form.Label>birth</Form.Label>
+    <Form.Control type="date" placeholder="Enter birth" />
+    </Form.Group>
+
+    <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+    </Form.Text>
+    </Form.Group>
+
+    <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+    <Button variant="primary" type="button" onClick={handleSubmit}>
+    Submit
+    </Button>
+  </Form>
 );
 
 export default Signup
