5f528a93e214958dd90934e95b78725ff4f5a541
justin <justin8953@gmail.com>
2019-02-18 13:35:04 +0000

    add recompose

--- web/routes/index.js
+++ web/routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 const users = require('../controller/user.js')
-router.get('/users', users.findAll);
+router.get('/users', users.findAll); 
+router.post('/users/create', users.create);
 // router.get('/users', users.findById);
 // router.put('/users', users.update);
 // router.create('/users', users.create);
