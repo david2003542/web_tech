d77fc9fde4bb3985b2289ff92bd6759ff6ec1f46
davidchen <david2003542@gmail.com>
2019-02-20 10:55:59 +0000

    add hook into sign up page

--- web_front/my-app/src/Page/SignComponent/Signup.js
+++ web_front/my-app/src/Page/SignComponent/Signup.js
@@ -1,65 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Form, Button } from 'react-bootstrap'
-import ReactDOM from 'react-dom'
 
-const state = {
-  ForeName: '',
-  LastName: '',
-  userName: '',
-  birth: '',
-  email: '',
-  password: ''
+
+// const [foreName, setForeName] = useState('')
+
+const Signup = () => {
+  const [foreName, setForeName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [userName, setUserName] = useState('')
+  const [birth, setBirth] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return(
+    <Form>
+      <Form.Group controlId="foreName">
+      <Form.Label>foreName</Form.Label>
+      <Form.Control type="text" placeholder="Enter foreName" onChange={e => setForeName(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="lastName">
+      <Form.Label>lastName</Form.Label>
+      <Form.Control type="text" placeholder="Enter lastName" onChange={e => setLastName(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="userName">
+      <Form.Label>userName</Form.Label>
+      <Form.Control type="text" placeholder="Enter userName" onChange={e => setUserName(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="birth">
+      <Form.Label>birth</Form.Label>
+      <Form.Control type="date" placeholder="Enter birth" onChange={e => setBirth(e.target.value)} />
+      </Form.Group>
+
+      <Form.Group controlId="email">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+      <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+      </Form.Text>
+      </Form.Group>
+
+      <Form.Group controlId="password">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="button">
+      Submit
+      </Button>
+    </Form>
+  )
 }
-
-const handleForeNameChange = (foreName) => {
-  state.foreName = foreName
-  this.setState(foreName)
-  // console.log(state)
-}
-
-
-const handleSubmit = () => {
-  console.log('submit')
-}
-
-const Signup = () => (
-  <Form>
-    <Form.Group controlId="formForeName">
-    <Form.Label>foreName</Form.Label>
-    <Form.Control type="text" placeholder="Enter foreName" ref="foreName" onChange={handleForeNameChange('ff')} />
-    </Form.Group>
-
-    <Form.Group controlId="formLastName">
-    <Form.Label>lastName</Form.Label>
-    <Form.Control type="text" placeholder="Enter lastName" />
-    </Form.Group>
-
-    <Form.Group controlId="userName">
-    <Form.Label>userName</Form.Label>
-    <Form.Control type="text" placeholder="Enter userName" />
-    </Form.Group>
-
-    <Form.Group controlId="birth">
-    <Form.Label>birth</Form.Label>
-    <Form.Control type="date" placeholder="Enter birth" />
-    </Form.Group>
-
-    <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-        We'll never share your email with anyone else.
-    </Form.Text>
-    </Form.Group>
-
-    <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-    <Button variant="primary" type="button" onClick={handleSubmit}>
-    Submit
-    </Button>
-  </Form>
-);
 
 export default Signup
