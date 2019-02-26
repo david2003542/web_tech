1c9c1f4c2e000038227d5c12ecbced7922711da9
davidchen <david2003542@gmail.com>
2019-02-26 15:25:22 +0000

    merge otto and me branch

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,16 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-<<<<<<< HEAD
-
-/* GET home page. */
-const users = require('../controller/user.js')
-router.get('/users', users.findAll);
-// router.get('/users', users.findById);
-// router.put('/users', users.update);
-// router.create('/users', users.create);
-// router.delete('/users', users.delete);
-=======
 const users = require('../controller/user.js')
 
 router.get('/users', users.findAll); 
@@ -18,7 +8,6 @@ router.get('/users/:id', users.findById);
 router.post('/users/create', users.create);
 router.put('/users/:id', users.update);
 router.delete('/users/:id', users.delete);
->>>>>>> d77fc9fde4bb3985b2289ff92bd6759ff6ec1f46
 
 
 
